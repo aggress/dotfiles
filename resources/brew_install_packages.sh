a60861aff9eb01feeb4688c9cc27a26d299e3e40
Richard Shaw <richard@aggress.net>
2014-03-11 14:31:30 +0000

    Add resources folder

---
+++ resources/brew_install_packages.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+BREW_PACKAGES="htop iftop links mercurial nmap par2 pstree subversion tmux unrar wget ngrep netcat mtr ansible saltstack homebrew/binary/packer ec2-ami-tools ec2-api-tools s3cmd pssh mpssh ctags boot2docker docker readline ttytter youtube-dl maven maven-shell p7zip"
+
+for BREW_PACKAGE in $BREW_PACKAGES
+do
+  brew install $BREW_PACKAGE
+done
