f976235c2d70bb564baeb8c3abe100d87f1bf958
Richard Shaw <richard@aggress.net>
2014-03-31 20:16:50 +0100

    Remove ansible, add easy_install

--- resources/brew_install_packages.sh
+++ resources/brew_install_packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BREW_PACKAGES="htop iftop links mercurial nmap par2 pstree subversion tmux unrar wget ngrep netcat mtr ansible saltstack homebrew/binary/packer ec2-ami-tools ec2-api-tools s3cmd pssh mpssh ctags boot2docker docker readline ttytter youtube-dl maven maven-shell p7zip reattach-to-user-namespace"
+BREW_PACKAGES="htop iftop links mercurial nmap par2 pstree subversion tmux unrar wget ngrep netcat mtr homebrew/binary/packer ec2-ami-tools ec2-api-tools s3cmd pssh mpssh ctags boot2docker docker readline ttytter youtube-dl maven maven-shell p7zip reattach-to-user-namespace easy_install"
 
 for BREW_PACKAGE in $BREW_PACKAGES
 do
